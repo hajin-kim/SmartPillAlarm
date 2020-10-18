f7270e3d4884d554039124c45531fc3a55a9b3cf
Hajin_Kim <kimhajin@yonsei.ac.kr>
2020-10-18 19:10:37 +0900

    API result debugging

--- app/src/main/java/com/example/smartpillalarm/MainActivity.java
+++ app/src/main/java/com/example/smartpillalarm/MainActivity.java
@@ -18,6 +18,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -33,7 +43,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.lang.reflect.Field;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private Button scan_button;
     private Context appContext;
     private FirebaseAuth firebaseAuth;
+    private StringBuilder requestResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -157,17 +170,17 @@ public class MainActivity extends AppCompatActivity {
             if(result.getContents() != null){
                 Toast.makeText(getApplicationContext(), result.getFormatName(), Toast.LENGTH_SHORT).show();
 
-                String codeFound = result.getContents();
+                String foundBarcodeDigits = result.getContents();
                 // get barcode format
                 switch (BarcodeFormat.valueOf(result.getFormatName())) {
                     case EAN_13:
-//                        codeFound = result.getContents();
-//                        Toast.makeText(appContext, BarcodeFormat.EAN_13 + " " + codeFound, Toast.LENGTH_SHORT).show();
+//                        foundBarcodeDigits = result.getContents();
+//                        Toast.makeText(appContext, BarcodeFormat.EAN_13 + " " + foundBarcodeDigits, Toast.LENGTH_SHORT).show();
                         break;
 
                     case DATA_MATRIX:
-                        codeFound = codeFound.substring(4, 4+13);
-//                        Toast.makeText(appContext, BarcodeFormat.DATA_MATRIX + " " + codeFound, Toast.LENGTH_SHORT).show();
+                        foundBarcodeDigits = foundBarcodeDigits.substring(4, 4+13);
+//                        Toast.makeText(appContext, BarcodeFormat.DATA_MATRIX + " " + foundBarcodeDigits, Toast.LENGTH_SHORT).show();
                         break;
 
                     default:
@@ -176,12 +189,11 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-                String response = "";
                 try {
-                    productCode = searchProdCode(codeFound);
+                    productCode = searchProdCode(foundBarcodeDigits);
                     System.out.println(productCode);
                     System.out.println("Start getAPI");
-                    response = Methods.getAPIResponse(getApplicationContext(), productCode);
+                    getAPIResponse(productCode);
                     System.out.println("Done getAPI");
                 } catch (IOException | IllegalAccessException e) {
                     System.out.println("Tracing getAPI");
@@ -190,33 +202,9 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(appContext, "Error found", Toast.LENGTH_SHORT).show();
 
                 }
-                System.out.println("TTT " + response.length());
-
-                Log.d(TAG, "This is actually Printing SOMETHING:"+response);
-                //parseXml(response);
-
-//                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage("제품코드: "+productCode);
-                builder.setTitle("스캔 결과");
-                builder.setPositiveButton("재시도", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        scanCode();
-                    }
-                }).setNegativeButton("완료", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //finish();
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
-//                Toast.makeText(appContext, codeFound, Toast.LENGTH_SHORT).show();
-                System.out.println("BARCODE TEST "+codeFound);
             }
             else{
-                Toast.makeText(this, "결과 없음", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Product code 결과 없음", Toast.LENGTH_SHORT).show();
             }
         }
         else{
@@ -225,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // XML string parser: gets XML as input in string
-    public void parseXml(String xmlString){
+    public void parseXml(String xmlString) throws IOException {
         try {
 
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -250,13 +238,11 @@ public class MainActivity extends AppCompatActivity {
 
         } catch (XmlPullParserException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
-    private Integer getRawID(String title) throws IllegalAccessException {
-        Integer RawID = 0;
+    private int getRawID(String title) throws IllegalAccessException {
+        int RawID = 0;
         Field[] fields=R.raw.class.getFields();
         for(int count=0; count < fields.length; count++){
             if(title.equals(fields[count].getName())){
@@ -270,7 +256,7 @@ public class MainActivity extends AppCompatActivity {
         String prodCode = "NULL";  // returns corresponding product code
         String title = barcode.substring(3,7);
         String key = barcode.substring(7);
-        Integer count = 0;
+        int count = 0;
 
         InputStream inputStream = getResources().openRawResource(getRawID("c" + title));
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
@@ -295,4 +281,113 @@ public class MainActivity extends AppCompatActivity {
         return prodCode;
     }
 
+    public void getAPIResponse(final String productCode) throws IOException {
+//        String[] serviceKey = {"r1uAyYmY3oR5pYyYwVAuUv%2FYqWVfqRmNNFWGEcsTqkABJmUp1CdLyPOWB5PLTnTQPGRduGwrjvr2Dxwp59mMYA%3D%3D", ""};
+//        String[] serviceKey = "r1uAyYmY3oR5pYyYwVAuUv%2FYqWVfqRmNNFWGEcsTqkABJmUp1CdLyPOWB5PLTnTQPGRduGwrjvr2Dxwp59mMYA%3D%3D".replace("%2F", "/").replace("%3D", "=");
+        String[] serviceKey = {"r1uAyYmY3oR5pYyYwVAuUv/YqWVfqRmNNFWGEcsTqkABJmUp1CdLyPOWB5PLTnTQPGRduGwrjvr2Dxwp59mMYA==", ""};
+
+        // TODO: it doesn't work
+        // to get API service key from firebase database
+//        final String[] serviceKey = new String[1];
+//        DatabaseReference databaseReference;
+//        databaseReference = FirebaseDatabase.getInstance().getReference("apiServiceKey");
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                serviceKey[0] = snapshot.getValue().toString();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        System.out.println(serviceKey[0]);
+
+        StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService/getMdcinPrductItem"); /*URL*/
+//        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=서비스키"); /*Service Key*/
+//        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=" + URLEncoder.encode(serviceKey, "UTF-8")); /*Service Key*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey[0]); /*Service Key*/
+//        urlBuilder.append("&" + URLEncoder.encode("item_name","UTF-8") + "=" + URLEncoder.encode("종근당염산에페드린정", "UTF-8")); /*품목명*/
+//        urlBuilder.append("&" + URLEncoder.encode("entp_name","UTF-8") + "=" + URLEncoder.encode("(주)종근당", "UTF-8")); /*업체명*/
+//        urlBuilder.append("&" + URLEncoder.encode("item_permit_date","UTF-8") + "=" + URLEncoder.encode("19550117", "UTF-8")); /*허가일자*/
+//        urlBuilder.append("&" + URLEncoder.encode("entp_no","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*업체허가번호*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("3", "UTF-8")); /*한 페이지 결과 수*/
+//        urlBuilder.append("&" + URLEncoder.encode("bar_code","UTF-8") + "=" + URLEncoder.encode("8806433032005", "UTF-8")); /*표준코드*/
+        urlBuilder.append("&" + URLEncoder.encode("item_seq","UTF-8") + "=" + URLEncoder.encode(productCode, "UTF-8")); /*품목기준코드*/
+//        urlBuilder.append("&" + URLEncoder.encode("start_change_date","UTF-8") + "=" + URLEncoder.encode("20151216", "UTF-8")); /*변경일자시작일*/
+//        urlBuilder.append("&" + URLEncoder.encode("end_change_date","UTF-8") + "=" + URLEncoder.encode("20160101", "UTF-8")); /*변경일자종료일*/
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(appContext);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlBuilder.toString(),
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        requestResult = new StringBuilder();
+                        requestResult.append( /*URLEncoder.encode(response, "EUC-KR")*/ response);
+//                        Toast.makeText(appContext, "Response is: "+ requestResult.toString(), Toast.LENGTH_SHORT).show();
+//                        System.out.println(requestResult.toString());
+                        succeedAPIRequest(productCode);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(appContext, "That didn't work!", Toast.LENGTH_SHORT).show();
+                System.out.println("That didn't work!");
+            }
+        })
+        {
+            @Override //response를 UTF8로 변경해주는 소스코드
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data, "UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (Exception e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                }
+            }
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+        stringRequest.setShouldCache(false);
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+//        Log.d(TAG, "HelloWorld! "+requestResult.toString());
+    }
+
+    public void succeedAPIRequest(String productCode) {
+        System.out.println("TTT " + requestResult.length());
+
+        Log.d(TAG, "This is actually Printing SOMETHING:"+requestResult);
+        //parseXml(response);
+
+//      Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("제품코드: "+productCode);
+        builder.setTitle("스캔 결과");
+        builder.setPositiveButton("재시도", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                scanCode();
+            }
+        }).setNegativeButton("완료", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+//      Toast.makeText(appContext, codeFound, Toast.LENGTH_SHORT).show();
+        System.out.println("BARCODE TEST "+productCode);
+    }
 }
